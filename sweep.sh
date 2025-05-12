4ea76fc593b0e79b4b1e1fce71f81b5a7a8b1f3d
jwass1997 <jacob.wasselewsky@stud.uni-heidelberg.de>
2025-05-12 14:00:49 +0200

    udpate

--- sweep.sh
+++ sweep.sh
@@ -2,7 +2,7 @@
 
 build() {
 	echo "Compiling..."
-	g++ -std=c++17 -O3 \
+	g++ -std=c++17 -Ofast \
 	FEMmethods.cpp SystemGraph.cpp Simulator.cpp utils.cpp main.cpp \
 	-o main \
 	-lmfem -lm -lcnpy -lz -fopenmp -lboost_program_options \
@@ -11,7 +11,7 @@ build() {
 
 run() {
 	echo "Starting to sweep..."
-	./main voltageSweep --numOfPoints=50 --equilibriumSteps=10000 --simulationSteps=100000 --fileName=test --min=-2.0 --max=2.0
+	./main voltageSweep --numOfPoints=100 --equilibriumSteps=10000 --simulationSteps=100000 --fileName=warmUp1e4_sim1e6_sig0.0_2 --min=-1.5 --max=1.5
 }
 
 build
